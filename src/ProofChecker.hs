a3da4e461148b60a82c29cd49d29df60a0fbfca7
Max Taldykin <jorpic@gmail.com>
2022-10-23 23:57:04 +0400

    Add implies and negate functions

--- src/ProofChecker.hs
+++ src/ProofChecker.hs
@@ -1,10 +1,11 @@
 module ProofChecker
-    ( unify
-    , unify'
-    , parseExpr
+    ( parseExpr
+    , unify, unify'
+    , negate
+    , implies
     ) where
 
-import Prelude hiding (Ordering(..))
+import Prelude hiding (Ordering(..), negate)
 import Control.Monad (foldM)
 import Data.List.Split (splitOn)
 import Data.Map (Map)
@@ -79,15 +80,15 @@ data Expr
   deriving (Show, Eq)
 
 
-guardE :: Bool -> String -> Either String ()
+guardE :: Bool -> String -> Result ()
 guardE c err = if c then Right () else Left err
 
-withContext :: String -> Either String a -> Either String a
+withContext :: String -> Result a -> Result a
 withContext cxt = \case
   Left err -> Left $ cxt <> unlines (map ("\n\t" <>) $ lines err)
   r -> r
 
-parseExpr :: String -> Either String Expr
+parseExpr :: String -> Result Expr
 parseExpr txt
   = withContext ("parsing expr: " <> txt)
   $ do
@@ -114,10 +115,11 @@ unify' p [q] = e2m $ unify p q
 unify' p qs  = e2m $ unify p $ Expr AN qs
 
 type UniMap = Map Char Char
+type Result a = Either String a
 
-unify :: Expr -> Expr -> Either String UniMap
+unify :: Expr -> Expr -> Result UniMap
 unify p q
-  = withContext ("unify:" <> show p <> " ~ " show q)
+  = withContext ("unify:" <> show p <> " ~ " <> show q)
   $ case (p,q) of
     (Atom x, Atom y)
       -> pure $ Map.singleton y x
@@ -128,7 +130,7 @@ unify p q
       foldM mergeMaps Map.empty uniMaps
     _ -> Left "cant' match functor with atom"
 
-mergeMaps :: UniMap -> UniMap -> Either String UniMap
+mergeMaps :: UniMap -> UniMap -> Result UniMap
 mergeMaps mx = go mx . Map.toList
   where
     go mx' [] = Right mx'
@@ -137,3 +139,30 @@ mergeMaps mx = go mx . Map.toList
         | vx == vy  -> go mx' ys
         | otherwise -> Left $ "conflict: " <> show (ky, [vx, vy])
       Nothing -> go (Map.insert ky vy mx') ys
+
+
+negate :: Expr -> Result Expr
+negate (Atom{}) = Left "can't negate an atom"
+negate (Expr fn xs) = case fn of
+  AN -> Expr OR <$> mapM negate xs
+  OR -> Expr AN <$> mapM negate xs
+  EQ -> pure $ Expr NE xs
+  NE -> pure $ Expr EQ xs
+  CO -> pure $ Expr NC xs
+  NC -> pure $ Expr CO xs
+  NO -> pure $ head xs
+  _  -> pure $ Expr NO [Expr fn xs]
+
+
+implies :: Expr -> Expr -> Expr -> Result Bool
+implies (Expr EQ [x,y]) (Expr f rs) (Expr g ps)
+  = pure
+  $  f == g
+  && length rs == length ps
+  && and [
+         (r == p)
+      || (r == x && p == y)
+      || (r == y && p == x)
+    | (r, p) <- zip rs ps
+    ]
+implies eq r p = Left $ "wrong args for implies" <> show (eq, r, p)
