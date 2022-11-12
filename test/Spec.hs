dc89c44e058488af76b7e6816ec5c5b09504e52e
Max Taldykin <jorpic@gmail.com>
2022-11-13 00:54:49 +0400

    Remove unification and unification tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,48 +1,8 @@
 {-# LANGUAGE DeriveGeneric #-}
 
-import Control.Monad (forM_)
-import Data.Aeson qualified as Aeson
-import Data.Map (Map)
-import Data.Text (Text)
-import Data.Text qualified as T
-import Data.ByteString.Lazy.Char8 qualified as B (lines, readFile)
-import GHC.Generics
 import Test.Hspec
-
 import ParserSpec qualified
-import ProofChecker (parseExpr, unify')
-
-data UnificationExample = UnificationExample
-  { p :: Text
-  , q :: [Text]
-  , res :: Maybe (Map Char Char)
-  } deriving (Show, Generic)
-
-instance Aeson.FromJSON UnificationExample
-
--- FIXME: add hand-made unification tests
 
 main :: IO ()
 main = do
   hspec ParserSpec.spec
-
-  -- this file was generated from tracing ./CheckProofs.php
-  jsonl <- B.lines <$> B.readFile "test/unification.jsonl"
-
-  hspec $ describe "unify"
-    $ forM_ jsonl $ \json -> maybe
-      (specify "..."
-        $ expectationFailure $ "invalid JSON in " <> show json)
-      unificationSpec
-      (Aeson.decode json)
-
-unificationSpec :: UnificationExample -> SpecWith ()
-unificationSpec (UnificationExample{..})
-  = specify (show p <> " ~ " <> show q) $ do
-    let res' = do
-          p' <- parseExpr $ T.unpack p
-          q' <- sequence $ map (parseExpr . T.unpack) q
-          pure $ unify' p' q'
-    case res' of
-      Left err -> expectationFailure err
-      Right atomMapping -> atomMapping `shouldBe` res
