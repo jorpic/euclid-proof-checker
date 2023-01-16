c1916f03e7cebca5929894283b16561ff1eb2ee7
Max Taldykin <jorpic@gmail.com>
2023-01-16 21:06:57 +0400

    add tests

--- test/ProofCheckerSpec.hs
+++ test/ProofCheckerSpec.hs
@@ -4,6 +4,7 @@ import Prelude hiding (Ordering(..))
 import Data.Map.Strict qualified as Map
 import Test.Hspec
 
+import Types
 import ProofChecker
 import TestUtils () -- for instance IsString Expr
 
@@ -32,19 +33,85 @@ spec = do
       $ rewriteAs "EQAA" "EQAB"
       `shouldBe`
         Left (StringErr ["conflicting mappings: ('A',('B','A'))"])
-    it "ok when no conflict "
+    it "ok when no conflict"
       $ rewriteAs "EQAB" "EQBA"
       `shouldBe`
         Right (Map.fromList [('A','B'), ('B','A')])
 
-  -- describe "checkProp" $ do
-  --   let eq = Fun EQ
-  --   it "infers expression from context" $ do
-  --     let cxt = [ eq "xy" , eq "yZ", eq "XY" , eq "YZ" , eq "XX" ]
-  --     let target = eq "XZ"
-  --     let prop = Prop [eq "AB", eq "BC", eq "CD"] "" (eq "AD")
-  --     -- AB -> xy
-  --     -- BC ->
-  --     checkProp cxt target prop
-  --       `shouldBe`
-  --         Right (Map.fromList [('A','X'),('B','X'),('C','Y'),('D','Z')])
+--   describe "rewriteAs'" $ do
+--     it "ok when no conflict"
+--       $ rewriteAs' "EQAB" "EQBA"
+--       `shouldBe`
+--         Right (Map.fromList [('A','B'), ('B','A')])
+--     it "ok when matching AN with AN"
+--       $ rewriteAs' "ANEQBX+EQXA" "ANEQBA+EQAC"
+--       `shouldBe`
+--         Right (Map.fromList [('B','B'), ('X','A'), ('A', 'C')])
+--     it "ok when matching AN with it's subexpr"
+--       $ rewriteAs' "ANEQBX+EQXA" "EQBA"
+--       `shouldBe`
+--         Right (Map.fromList [('B','B'), ('X','A')])
+--     it "fails when matching OR with it's subexpr"
+--       $ rewriteAs' "OREQBX|EQXA" "EQBA"
+--       `shouldBe`
+--         Left (StringErr ["can't match OR[EQBX,EQXA] with EQBA"])
+--     it "ok when matching subexpr with OR"
+--       $ rewriteAs' "EQBA" "OREQBX|EQXA"
+--       `shouldBe`
+--         Right (Map.fromList [('A','X'), ('B','B')])
+--     it "fails when matching subexpr with AN"
+--       $ rewriteAs' "EQBA" "ANEQBX+EQXA"
+--       `shouldBe`
+--         Left (StringErr ["can't match EQBA with AN[EQBX,EQXA]"])
+
+
+  let can = (`shouldBe` Right ())
+  let can't = (`shouldNotBe` Right ())
+
+  describe "inferExact" $ do
+    let cxt = ["BEXYZ", "BEABC", "BEDFG", "BEFGH"]
+    it "finds expr in context"
+      $ can $ inferExact cxt "BEDFG"
+    it "can't find subexpressions of AN"
+      $ can't $ inferExact cxt "ANBEDFG+BEFGH"
+
+  describe "inferExact'" $ do
+    let cxt = ["BEXYZ", "BEABC", "BEDFG","BEFGH"]
+    it "finds expr in context"
+      $ can $ inferExact' cxt "BEDFG"
+    it "can find subexpressions of AN"
+      $ can $ inferExact' cxt "ANBEDFG+BEFGH"
+    it "can find subexpressions of OR"
+      $ can $ inferExact' cxt "ORBEDFG|BEQWE"
+
+  describe "inferWithEqs" $ do
+    let cxt = ["EQAX", "EQYB", "BEAWY", "COABX"]
+    it "find expr with equality substitutions"
+      $ can $ inferWithEqs cxt "BEXWB"
+    it "can't find subexpr"
+      $ can't $ inferWithEqs cxt "ANBEXWB+COXYX"
+
+  describe "inferWithEqs'" $ do
+    let cxt = ["EQAX", "EQYB", "BEAWY", "COABX"]
+    it "can find subexpr of AN"
+      $ can $ inferWithEqs' cxt "ANBEXWB+COXYX"
+    it "can find subexpr of OR"
+      $ can $ inferWithEqs' cxt "ORBEZXC|COXYX"
+    it "can find deeply nested subexpr of AN"
+      $ can $ inferWithEqs' cxt "ANBEXWB+ORBEZXC|COXYX"
+    it "can find deeply nested subexpr of OR"
+      $ can $ inferWithEqs' cxt (OR ["ANBEXWB+COXYX", "BEZXC"])
+
+  describe "inferWithProp'" $ do
+    let cxt = ["BEXYZ"]
+    it "can use conjunct in consequent"
+      $ can $ inferWithProp'
+        cxt
+        (Prop ["BEABC"] "" "ANNEBC+NEAB+NEAC" False)
+        "NEXY"
+
+--    - we have BEXYZ in the context
+--    - we want to prove NEXY using prop BEABC => AN[NEBC,NEAB,NEAC]
+--    - it is sufficient to match NEAB with NEXY
+--
+--    AN + OR
